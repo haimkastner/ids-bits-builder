e93ba57e95ea49b3c5de405ca59200f42f7a4226
Haim Kastnet <haim.kastner@gmail.com>
2019-11-07 21:53:49 +0200

    Upload old code Convert a number to a bits array in the little-endian method.

--- ids_builder.cpp
+++ ids_builder.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <bitset>
+
 int main(int argc, const char *argv[])
 {
-    std::cout << "Hello IDS bits builder C++ Code!" << '\n';
+    int n;
+
+	n = 0xA513;
+
+	std::bitset<16> vec;
+
+	for (size_t i = 15; i >= 0; i--)
+	{
+		vec[i] = n % 2;
+		n = n / 2;
+		if (n <= 0)
+			break;
+	}
+
     return 0;
 }
